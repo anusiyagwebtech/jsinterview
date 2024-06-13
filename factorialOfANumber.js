9796ca21f23b732c485ee4a82735dc5742881064
anusiyagwebtech <141471962+anusiyagwebtech@users.noreply.github.com>
2024-06-13 02:45:04 -0700

    Update factorialOfANumber.js

--- factorialOfANumber.js
+++ factorialOfANumber.js
@@ -6,3 +6,20 @@
     }
 
     console.log(FactorialOfANumber(8));
+
+
+
+
+
+    const FactorialOfANumberFor = (number) => {
+      let factor = 1;
+      for (let index = 1; index <= number; index++) {
+        factor *= index;
+        
+      }
+      return factor;
+    }
+
+    console.log(FactorialOfANumberFor(3));
+
+    
