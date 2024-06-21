fcc6f1c6a1db6e64072b4697bde13dc6dd11003a
anusiyagwebtech <141471962+anusiyagwebtech@users.noreply.github.com>
2024-06-20 23:56:17 -0700

    Update selectionSort.js

--- selectionSort.js
+++ selectionSort.js
@@ -26,3 +26,31 @@ const a = [8,7,6,5,4,3,2,1,32,-5,9];
   
 
    console.log(selectionSortAlgorithm(a));
+
+
+ const selectionSortAlgorithmInBook = (a) => {
+    let steps = 0;
+   for (let k = 0; k < a.length; k++) {
+    
+    let j = k;
+    for (let i = k+1; i < a.length; i++) {
+     steps++;
+    if (a[i] < a[k]) {
+      
+      j = i;
+    }
+   }
+   if (k != j) {
+    let temp = a[k];
+    a[k] = a[j];
+    a[j] = temp;
+   }
+   
+   }
+  
+   console.log(steps);
+   return a;
+   }
+
+
+   console.log(selectionSortAlgorithmInBook(a));
