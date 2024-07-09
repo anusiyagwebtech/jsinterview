0bb3b9a59f1e305ec7baf4f245742dc891dd0b13
anusiyagwebtech <141471962+anusiyagwebtech@users.noreply.github.com>
2024-07-09 22:30:38 +0530

    Update lastbutnotleast.js

--- lastbutnotleast.js
+++ lastbutnotleast.js
@@ -31,3 +31,13 @@
      }
 
      console.log(lastButNotLeast(454, 34, 87));
+
+
+
+
+function lastButNotLeast(a,b, c) {
+      a = a%10;
+      b = b%10;
+      c = c%10;
+      return a == b || b == c || a == c;
+     }
